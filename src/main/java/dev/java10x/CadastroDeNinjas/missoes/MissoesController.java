c89178823034213da0e768e6e6e6aa737a8ea965
cleber <silva1cleber@gmail.com>
2025-08-20 10:26:35 -0300

    feat: Ciar as rotas do Controller de Missoes com mock

--- src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesController.java
@@ -1,4 +1,28 @@
 package dev.java10x.CadastroDeNinjas.missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissoes() {
+        return "Missoes listadas";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missao criada";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missão alterada";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missão deletada";
+    }
 }
