be3892a5357dd3649d85f63c742e5ca7c8ba9a61
cleber <silva1cleber@gmail.com>
2025-08-22 10:04:37 -0300

    feat: Cria as rotas de lista de Missoes e Cria Missoes

--- src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesController.java
@@ -2,25 +2,41 @@ package dev.java10x.CadastroDeNinjas.missoes;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/missoes")
 public class MissoesController {
 
-    @GetMapping("/listar")
-    public String listarMissoes() {
-        return "Missoes listadas";
+    public MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
     }
 
+    // CRUD de Missoes
+    // Criar nova missao  -- CREATE
     @PostMapping("/criar")
-    public String criarMissao() {
-        return "Missao criada";
+    public MissoesModel criarMissao(@RequestBody MissoesModel missao) {
+        return missoesService.ciarMissao(missao);
     }
 
+    // Listar todas as missoes -- READ
+    @GetMapping("/listar")
+    public List<MissoesModel> listarMissoes() {
+        return missoesService.listarMissoes();
+    }
+
+    // Listar Missoes por ID -- READ
+
+
+    // Alterar Missao -- UPDATE
     @PutMapping("/alterar")
     public String alterarMissao() {
         return "Missão alterada";
     }
 
+    // Deletar Missao -- DELETE
     @DeleteMapping("/deletar")
     public String deletarMissao() {
         return "Missão deletada";
--- src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesService.java
@@ -1,4 +1,31 @@
 package dev.java10x.CadastroDeNinjas.missoes;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    // Criar Missoes
+    public MissoesModel ciarMissao(MissoesModel missao) {
+        return missoesRepository.save(missao);
+    }
+
+    // Listar todas as missoes
+    public List<MissoesModel> listarMissoes() {
+        return missoesRepository.findAll();
+    }
+
+    // Listar missao por ID
+
+
+
 }
