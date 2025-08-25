29a89b89300819d12cb5d7ad40da718a4283804c
cleber <silva1cleber@gmail.com>
2025-08-25 09:36:05 -0300

    feat: Cria o DTO e Mapper para as Missoes

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesDTO.java
@@ -0,0 +1,22 @@
+package dev.java10x.CadastroDeNinjas.missoes;
+
+import dev.java10x.CadastroDeNinjas.ninjas.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MissoesDTO {
+
+    private Long id;
+
+    private String missao;
+
+    private String dificuldade;
+
+    private List<NinjaModel> ninjas;
+}
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesMapper.java
@@ -0,0 +1,26 @@
+package dev.java10x.CadastroDeNinjas.missoes;
+
+public class MissoesMapper {
+
+    MissoesModel map(MissoesDTO missoesDTO) {
+        MissoesModel missoesModel = new MissoesModel();
+
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setMissao(missoesDTO.getMissao());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+        missoesModel.setNinjas(missoesDTO.getNinjas());
+
+        return missoesModel;
+    }
+
+    MissoesDTO map(MissoesModel missoesModel) {
+        MissoesDTO missoesDTO = new MissoesDTO();
+
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setMissao(missoesModel.getMissao());
+        missoesDTO.setDificuldade(missoesModel.getDificuldade());
+        missoesDTO.setNinjas(missoesModel.getNinjas());
+
+        return missoesDTO;
+    }
+}
