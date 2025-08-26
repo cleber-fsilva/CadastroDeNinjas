b8ebed1803a559b084b054b4c307d0d8f1bced9e
cleber <silva1cleber@gmail.com>
2025-08-26 15:15:34 -0300

    refactor: Inclui a annotation ToString no models de Ninja e Missoes

--- src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesModel.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class MissoesModel {
 
     @Id
--- src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaModel.java
@@ -10,6 +10,7 @@ import lombok.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "missoes")
 public class NinjaModel {
 
 
